66d52af642dc6069689a66c770ac69f7a45e9a7a
panto <pantostado1601@gmail.com>
2023-05-05 01:06:27 -0400

    Creacion Dulces y DepositoDulces

--- DepositoDulces.java
+++ DepositoDulces.java
@@ -1,2 +1,25 @@
+import java.util.ArrayList;
+
 public class DepositoDulces {
+
+    private ArrayList<Dulces> deposito;
+
+    public DepositoDulces() {
+        deposito = new ArrayList<Dulces>();
+    }
+
+    public void addDulces(Dulces Dulces) { // agrega Dulces al ArrayList<Dulces> deposito
+        deposito.add(Dulces);
+    }
+
+    public Dulces getDulces() { // devuelve un dulce en caso de que hayan en el deposito, en caso contrario
+        // retorna `null`
+        if (deposito.isEmpty()) {
+            return null;
+        } else {
+            Dulces Dulces = deposito.get(0);
+            deposito.remove(0);
+            return Dulces;
+        }
+    }
 }
--- Dulces.java
+++ Dulces.java
@@ -1,2 +1,36 @@
-public class Dulces {
+abstract class Dulces {
+    private int serie;
+
+    public Dulces(int serie) {
+        this.serie = serie;
+    }
+
+    public int getSerie() {
+        return serie;
+    }
+
+    public abstract String comer();
+}
+
+class Super8 extends Dulces {
+
+    public Super8(int serie) {
+        super(serie);
+    }
+
+    public String comer() {
+        return "super8";
+    }
+}
+
+class Snickers extends Dulces {
+
+    public Snickers(int serie) {
+        super(serie);
+    }
+
+    public String comer() {
+        return "snickers";
+    }
+
 }
