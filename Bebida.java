edac2939d49198462662c9e1d33e4bd1be7b7b14
tdos25 <torodriguez2021@udec.cl>
2023-05-04 23:43:10 -0400

    implementacion de la clase Moneda, Bebida y depositoBebida

--- Bebida.java
+++ Bebida.java
@@ -1,2 +1,36 @@
-public class Bebida {
+abstract class Bebida {
+
+    private int serie;
+
+    public Bebida(int serie) {
+        this.serie = serie;
+    }
+
+    public int getSerie() {
+        return serie;
+    }
+
+    public abstract String beber();
+}
+
+class CocaCola extends Bebida {
+
+    public CocaCola(int serie) {
+        super(serie);
+    }
+
+    public String beber() {
+        return "cocacola";
+    }
+}
+
+class Sprite extends Bebida {
+
+    public Sprite(int serie) {
+        super(serie);
+    }
+
+    public String beber() {
+        return "sprite";
+    }
 }
--- Moneda.java
+++ Moneda.java
@@ -1,2 +1,42 @@
-public class Moneda {
+abstract class Moneda {
+
+    public Moneda() {
+    }
+
+    public String getSerie() { // retorna su dirección en RAM como número de serie
+        return "" + this.hashCode();
+    }
+
+    public String toString() { // imprime el valor de la moneda y la direccion de memoria ram
+        return "Valor de la moneda " + getValor() + " Nro. de serie: " + getSerie();
+    }
+
+    public abstract int getValor(); // retorna la cantidad que vale la moneda
+}
+
+class Moneda1000 extends Moneda {
+    public Moneda1000() {
+    }
+
+    public int getValor() {
+        return 1000;
+    }
+}
+
+class Moneda500 extends Moneda {
+    public Moneda500() {
+    }
+
+    public int getValor() {
+        return 500;
+    }
+}
+
+class Moneda100 extends Moneda {
+    public Moneda100() {
+    }
+
+    public int getValor() {
+        return 100;
+    }
 }
